9341ffcf4b37d4a5639ed28518f03cce57359c22
Matt Galvin <matthew.galvin@erlang-solutions.com>
2025-02-15 12:33:13 +0100

    Experiment with one pipeline

--- steganography.exs
+++ steganography.exs
@@ -8,9 +8,18 @@ defmodule Steganography do
 
     <<_::binary-size(pixel_offset), pixel_data::binary>> = binary
 
-    pixel_data
-    |> extract_pixels(width, height)
-    |> Stream.flat_map(& &1)
+    row_size = div(width * 3 + 3, 4) * 4
+
+    Stream.flat_map((height - 1)..0//-1, fn row ->
+      Stream.flat_map(0..(width - 1), fn col ->
+        pixel_start = row * row_size + col * 3
+
+        <<_::binary-size(pixel_start), _b::7, b::1, _g::7, g::1, _r::7, r::1, _::binary>> =
+          pixel_data
+
+        [r, g, b]
+      end)
+    end)
     |> Stream.chunk_every(8)
     |> Stream.map(fn bits ->
       for bit <- bits, reduce: 0 do
@@ -21,19 +30,6 @@ defmodule Steganography do
     |> Stream.reject(&(&1 == <<0>>))
     |> Stream.into(File.stream!("decode.txt", [:write]))
     |> Stream.run()
-  end
-
-  defp extract_pixels(pixel_data, width, height) do
-    row_size = div(width * 3 + 3, 4) * 4
-
-    for row <- (height - 1)..0//-1, col <- 0..(width - 1), into: [] do
-      pixel_start = row * row_size + col * 3
-
-      <<_::binary-size(pixel_start), _b::7, b::1, _g::7, g::1, _r::7, r::1, _::binary>> =
-        pixel_data
-
-      [r, g, b]
-    end
   end
 end
 
